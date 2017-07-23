3a4d8b928a00694b07b163991a74a2c2eabb1ab3
vinay manyam <vinayip541@gmail.com>
2017-07-23 12:55:50 +0530

    Delete error4.php

--- application/view/error4.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-danger" role="alert">
-					<h3><strong>Login Failed ! </strong>your account is blocked by administrator,please try again later
-                                         
-				</div></h3>
