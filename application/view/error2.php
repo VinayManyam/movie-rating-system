d0c77d49e59693561a36190f94b2c4b1e60e0005
vinay manyam <vinayip541@gmail.com>
2017-07-23 12:54:35 +0530

    Delete error2.php

--- application/view/error2.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-danger" role="alert">
-					<h3><strong>Registration Failed</strong> validation failed
-                                           <?= validation_errors()?>
-				</div></h3>
