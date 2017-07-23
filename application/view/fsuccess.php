77d043c5cc5ff0503d07e4c63d4c4d4bec2bd6cc
vinay manyam <vinayip541@gmail.com>
2017-07-23 12:55:18 +0530

    Delete fsuccess.php

--- application/view/fsuccess.php
+++
@@ -1,4 +0,0 @@
-
-                                <div class="alert alert-success" role="alert">
-				<h3><strong>SUCCESS</strong> Your movie is successfully uploaded and waiting for verification team reviews 
-				</div></h3>
